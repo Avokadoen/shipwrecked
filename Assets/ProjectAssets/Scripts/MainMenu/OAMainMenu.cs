12df951316fca4b5a31b17037eb13a407ee9d4a1
avokadoen <akselhallajs@gmail.com>
2020-09-19 16:40:43 +0200

    Placeholder main menu

---
+++ Assets/ProjectAssets/Scripts/MainMenu/OAMainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OAMainMenu : MonoBehaviour
+{
+    public void OnPlayBtnClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnQuitBtnClick()
+    {
+        // Only works when built
+        Application.Quit();
+    }
+}
