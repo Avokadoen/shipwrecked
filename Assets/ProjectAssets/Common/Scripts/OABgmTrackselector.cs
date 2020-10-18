2343e75c9287601829b99c04a4c7c627ccf629a0
Ajanthan <ajanthaj@stud.ntnu.no>
2020-10-18 17:50:29 +0200

    music and testsound added. Updated barricades aswell

---
+++ Assets/ProjectAssets/Common/Scripts/OABgmTrackselector.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class OABgmTrackselector : MonoBehaviour
+{
+
+  
+
+    [SerializeField]
+    private List<AudioClip> tracks;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private Vector2 breakRange;
+
+    private float trackPlayedDuration;
+    private float trackLength;
+    private float trackBreak;
+    private int previousTrack = -1;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!audioSource)
+        {
+            audioSource = GetComponent<AudioSource>(); // adds audioSource if it's missing
+
+
+        }
+
+        if (tracks.Count < 2)
+        {
+            Debug.LogError("OABgmTrackselector need atleast 2 tracks!");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trackPlayedDuration += Time.deltaTime;
+
+        if (trackPlayedDuration < trackLength + trackBreak)
+            return;
+
+        var trackSelector = previousTrack;
+        while (trackSelector == previousTrack)
+        {
+            trackSelector = Random.Range(0, tracks.Count);
+        }
+        previousTrack = trackSelector;
+        audioSource.clip = tracks[trackSelector];
+        trackPlayedDuration = 0;
+        trackLength = tracks[trackSelector].length;
+        trackBreak = Random.Range(breakRange.x, breakRange.y);
+
+        audioSource.Play();
+        
+    }
+}
