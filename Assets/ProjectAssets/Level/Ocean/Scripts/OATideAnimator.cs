1a813d552238f921d6a63b35065da3fb3cd10f68
avokadoen <akselhallajs@gmail.com>
2020-10-22 14:57:50 +0200

    Refactor tide system, events on tide state
    Tide are more logical coupled with low and high tide. Event are now emitted to observers when tide becomes low/high

--- Assets/ProjectAssets/Level/Ocean/Scripts/OATideAnimator.cs
+++ Assets/ProjectAssets/Level/Ocean/Scripts/OATideAnimator.cs
@@ -1,23 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+/// <summary>
+/// Script to animate a tide cycle. 
+/// This script is also capable of calling functions when low/high- tide begins
+/// </summary>
 public class OATideAnimator : MonoBehaviour
 {
     [Tooltip("How the water will move towards the lowTide")]
     [SerializeField]
     private AnimationCurve tideCycle;
 
+    // TODO: find a more user friendly way of defining this
+    [Tooltip("When the animation is above this value, the ocean is in high tide")]
+    [SerializeField]
+    private float lowTideThreshold = 0.37f;
+
     [Tooltip("How low the water will be at its lowest")]
     [SerializeField]
+    [Min(0.1f)]
     private float lowTideDecline = 10f;
 
     [Tooltip("How long a cycle takes in total in seconds")]
     [SerializeField]
     private float cycleDuration = 180;
 
+    [Tooltip("Delegate of low tide event")]
+    [SerializeField]
+    UnityEvent onLowTide;
+
+    [Tooltip("Delegate of high tide event")]
+    [SerializeField]
+    UnityEvent onHighTide;
+
     private float cycleDurationPos;
     private float cycleDir = 1;
+    private bool wasUnderLowTideThresholdPrevious = false;
     private Vector3 startPos;
     private Vector3 updatePos;
 
@@ -27,25 +47,55 @@ public class OATideAnimator : MonoBehaviour
         // Negative values does not really make sense here, so we abs the value
         lowTideDecline = Mathf.Abs(lowTideDecline);
 
+
         cycleDurationPos = 0;
         startPos = transform.position;
+        startPos.y -= lowTideDecline;
+        transform.position = startPos;
+
+        onLowTide.AddListener(Test1);
+        onHighTide.AddListener(Test2);
     }
 
-    void FixedUpdate()
+    void Test1()
     {
-        // We start with low-tide, so we do -1 
-        float tideState = 1 - tideCycle.Evaluate(cycleDurationPos / cycleDuration);
+        Debug.Log("Lowtide!");
+    }
+
+    void Test2()
+    {
+        Debug.Log("Hightide!");
+    }
+
+    void Update()
+    {
+        float tideState = tideCycle.Evaluate(cycleDurationPos / cycleDuration);
         
         updatePos = startPos;
-        updatePos.y = startPos.y - (lowTideDecline * tideState);
+        updatePos.y = startPos.y + (lowTideDecline * tideState);
         transform.position = updatePos;
+
+        // TODO: maybe refactor this bool hell?
+        // Announce tide state if it changes one way or another
+        bool isUnderThreshold = tideState <= lowTideThreshold;
+        if (isUnderThreshold && !wasUnderLowTideThresholdPrevious)
+        {
+            Debug.Log(tideState);
+            onLowTide.Invoke();
+        }
+        else if (!isUnderThreshold && wasUnderLowTideThresholdPrevious)
+        {
+            Debug.Log(tideState);
+            onHighTide.Invoke();
+        }
+        wasUnderLowTideThresholdPrevious = isUnderThreshold;
 
         // If we are moving from low -> high tide or vice versa
         if (cycleDurationPos >= cycleDuration)
             cycleDir = -1;
-        else if (cycleDurationPos <= 0) 
+        else if (cycleDurationPos <= 0)
             cycleDir = 1;
 
-        cycleDurationPos += (Time.fixedDeltaTime * cycleDir);
+        cycleDurationPos += (Time.deltaTime * cycleDir);
     }
 }
