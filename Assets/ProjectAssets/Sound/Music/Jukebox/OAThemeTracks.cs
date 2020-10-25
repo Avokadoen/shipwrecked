1fec88c93a45df02cd9ddc0272c8461d5912fa5c
avokadoen <akselhallajs@gmail.com>
2020-10-25 12:14:49 +0100

    Fundementals for background music
    This also does some refactoring of main menu. Buttons now indicate hovering. Use a different placeholder background

---
+++ Assets/ProjectAssets/Sound/Music/Jukebox/OAThemeTracks.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ThemeTracks", menuName = "OceanAlien/ThemeTracks", order = 2)]
+public class OAThemeTracks : ScriptableObject
+{
+    [Tooltip("Random pause between each track, x is lowest, y is highes possible pause")]
+    public Vector2 pauseRndRange;
+
+    [Tooltip("All songs that should be grouped together i,e 'all main menu music'")]
+    public List<AudioClip> clips;
+
+    [Tooltip("How each song should fade in")]
+    public AnimationCurve fadeIn;
+
+    [Tooltip("How each song should fade out")]
+    public AnimationCurve fadeOut;
+}
