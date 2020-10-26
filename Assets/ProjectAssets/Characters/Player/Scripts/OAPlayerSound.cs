bc81ccc3f626a7882fd0433d9502b6c05c3e5c50
Arin von Krogh <avkrogh5@gmail.com>
2020-10-26 11:46:10 +0100

    working with sounds

---
+++ Assets/ProjectAssets/Characters/Player/Scripts/OAPlayerSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OAPlayerSound : MonoBehaviour
+{
+
+	[SerializeField]
+	private OAAudioPlayer ground;
+
+	[SerializeField]
+	private OAAudioPlayer water;
+ 
+    public void PlayFootstepSound() 
+    {
+		ground.PlayRandomClip();
+    }
+
+   public void PlaySwimmingSound() 
+    {
+		water.PlayRandomClip();
+    }
+}
