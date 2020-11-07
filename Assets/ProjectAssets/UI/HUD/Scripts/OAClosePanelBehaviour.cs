2db7bdef638e8a9f1d611c8cc7eff7cb836902d4
avokadoen <akselhallajs@gmail.com>
2020-11-07 16:30:22 +0100

    Further work on message system + animations

---
+++ Assets/ProjectAssets/UI/HUD/Scripts/OAClosePanelBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OAClosePanelBehaviour : StateMachineBehaviour
+{
+    GameObject profilePanel;
+    GameObject textPanel;
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!profilePanel || !textPanel)
+        {
+            var panelScript = animator.GetComponent<OAMessagePanel>();
+            profilePanel = panelScript.ProfilePanel;
+            textPanel = panelScript.TextPanel;
+        }
+        profilePanel.SetActive(false);
+        textPanel.SetActive(false);
+    }
+
+}
--- Assets/ProjectAssets/UI/HUD/Scripts/OAMessagePanel.cs
+++ Assets/ProjectAssets/UI/HUD/Scripts/OAMessagePanel.cs
@@ -2,52 +2,95 @@
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
+// TODO: the dialog should be a state machine where we always go through tutorial state (maybe add options in setting to skip)
 [RequireComponent(typeof(OAAudioPlayer))]
 public class OAMessagePanel : MonoBehaviour
 {
+    public enum Status
+    {
+        Close = 0,
+        Open = 1,
+        Idle = 2
+    }
+
     [SerializeField]
     TMPro.TextMeshProUGUI messageText;
 
+    [Tooltip("Delay between each character in a word")]
     [SerializeField]
     float charDelay = 0.05f;
 
+    [Tooltip("Delay between each space")]
     [SerializeField]
     float spaceDelay = 0.15f;
 
+    [Tooltip("Delay between each punctation ('.', ',', '?')")]
+    [SerializeField]
+    float punctationDelay = 0.5f;
+
+    [Tooltip("How long the panel stay up after all text is displayed")]
+    [SerializeField]
+    float closePanelDelay = 4f;
+
+    [SerializeField]
+    GameObject profilePanel;
+    public GameObject ProfilePanel { get => profilePanel; }
+
+    [SerializeField]
+    GameObject textPanel;
+    public GameObject TextPanel { get => textPanel; }
+
+    [SerializeField]
+    Animator anim;
+
     private OAAudioPlayer audioPlayer;
     private AudioSource source;
+
 
     void Start()
     {
         audioPlayer = GetComponent<OAAudioPlayer>();
         source = GetComponent<AudioSource>();
+
+        anim.SetInteger("status", (int)Status.Idle);
     }
 
     [ContextMenu("test")]
     void test()
     {
-        SetMessage("This is a test of the set message. Does it work? I don't know. Let's find out");
+        SetMessage("This is a test of the set message. Does it work? I don't know. Let's find out. I make this string long to also test the playing of audio");
     }
 
     public void SetMessage(string message)
     {
+        profilePanel.SetActive(true);
+        textPanel.gameObject.SetActive(true);
+
+
+        StopAllCoroutines();
         StartCoroutine(WriteMessage(message));
     }
 
+    // SPAGHETTI WARNING!!! ! :'( 
+    // TODO: rewrite all of this
     IEnumerator WriteMessage(string message)
     {
+        anim.SetInteger("status", (int)Status.Open);
+
         messageText.text = "";
         int pos = 0;
         var sb = new StringBuilder("", message.Length);
 
-        var audioDuration = 0;
+        var audioDuration = 0f;
+        var prev_iter_time = Time.time;
         float playDialogSound()
         {
             audioPlayer.PlayRandomClip();
             audioDuration = 0;
-            return source.clip.length;
+            return source.clip.length * source.pitch;
         }
         var audioLength = playDialogSound();
 
@@ -57,22 +100,40 @@ public class OAMessagePanel : MonoBehaviour
             {
                 audioLength = playDialogSound();
             }
+            audioDuration += Time.time - prev_iter_time;
+            prev_iter_time = Time.time;
 
             sb.Append(message[pos]);
 
             // TODO: this creates a lot of garbage (I think?)
             messageText.text = sb.ToString();
-            if (message[pos] == ' ')
+
+            switch (message[pos])
             {
-                yield return new WaitForSeconds(spaceDelay);
-            } else
-            {
-                yield return new WaitForSeconds(charDelay);
+                case ' ':
+                    yield return new WaitForSeconds(spaceDelay);
+                    break;
+                case '.':
+                case '?':
+                case ',':
+                    source.Pause();
+                    var delay = punctationDelay * .5f;
+                    yield return new WaitForSeconds(delay);
+                    source.Play();
+                    yield return new WaitForSeconds(delay);
+                    break;
+                default:
+                    yield return new WaitForSeconds(charDelay);
+                    break;
             }
 
             pos += 1;
         }
 
         source.Stop();
+
+        yield return new WaitForSeconds(closePanelDelay);
+
+        anim.SetInteger("status", (int)Status.Close);
     }
 }
