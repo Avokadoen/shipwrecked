f6841df4c6ef41c7d3cea009fc60bc9f5447a38f
avokadoen <akselhallajs@gmail.com>
2020-11-11 22:00:34 +0100

    WIP intro scene

---
+++ Assets/ProjectAssets/Intro/OAComicBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OAComicBehaviour : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        animator.SetBool("getNext", false);
+    }
+}
