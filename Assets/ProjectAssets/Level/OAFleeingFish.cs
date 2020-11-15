7383ff8c456577760b2de8db7888fce65c4fff87
avokadoen <akselhallajs@gmail.com>
2020-11-15 21:21:42 +0100

    added fleeing fish and removed unused scene object

---
+++ Assets/ProjectAssets/Level/OAFleeingFish.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OAFleeingFish : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector2 endPoint;
+
+    [Tooltip("how long fish should take to reach end point in seconds")]
+    [SerializeField]
+    float totalFleeTime = 4f;
+    float currentFleeTime = 0f;
+
+    [Tooltip("how long fish should wait before fleeing in seconds")]
+    [SerializeField]
+    float fleeDelay = 2f;
+    float seeTime = 0f;
+
+    Vector2 startPoint;
+
+    bool isFleeing = false;
+
+    private void Start()
+    {
+        startPoint = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isFleeing)
+            return;
+
+        if (seeTime < fleeDelay)
+        {
+            seeTime += Time.fixedDeltaTime;
+            return;
+        }
+
+        transform.position = Vector2.Lerp(startPoint, endPoint, currentFleeTime / totalFleeTime);
+
+        if (currentFleeTime > totalFleeTime)
+        {
+            Destroy(gameObject);
+
+            isFleeing = false;
+
+            return;
+        }
+
+        currentFleeTime += Time.fixedDeltaTime;
+    }
+
+    void OnBecameVisible()
+    {
+        // TODO: we could also just check the distance to the player
+        isFleeing = true;
+    }
+}
