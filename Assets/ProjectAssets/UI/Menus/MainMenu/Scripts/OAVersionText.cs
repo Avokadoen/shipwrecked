c40b8c75546508736d58018eb265c2a41dc3c8c9
avokadoen <akselhallajs@gmail.com>
2020-11-18 21:15:12 +0100

    #70: added version number to main menu

---
+++ Assets/ProjectAssets/UI/Menus/MainMenu/Scripts/OAVersionText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TMPro.TextMeshProUGUI))]
+public class OAVersionText : MonoBehaviour
+{
+    void Start()
+    {
+        var versionText = GetComponent<TMPro.TextMeshProUGUI>();
+        versionText.text = $"Version: {Application.version}";
+    }
+}
