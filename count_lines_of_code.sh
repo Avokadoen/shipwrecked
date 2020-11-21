06bb35d133fe2399b2532132d02cd426b7b3230a
avokadoen <akselhallajs@gmail.com>
2020-11-21 17:57:39 +0100

    Made count lines count shade code also

--- count_lines_of_code.sh
+++ count_lines_of_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Source: https://stackoverflow.com/a/1358573/11768869
-find ./Assets/ProjectAssets/ -name '*.cs' | xargs wc -l
+find ./Assets/ProjectAssets/ -name '*.cs' -o -name '*.shader' | xargs wc -l
